f5cd47aa9d33575aa33f2789b852f2dac36e2670
Nico P <nicopinsard@gmail.com>
2017-03-24 00:32:58 +0100

    bla bla bla bla

--- index.php
+++ index.php
@@ -3,8 +3,17 @@ include 'header.php';
 ?>
 <div class="container">
     <div class="row">
+        <div class="text-acceuil col-xs-12 text-center"
+             <p>
+                 Choisissez vos produits,<br/>
+                 Choisissez votre sport,<br/>
+                 Nous vous dirons combien de temps vous devez pratiquer ce sport pour éliminer les calories ...
+             </p>
+        </div>
+    </div>
+    <div class="row">
         <form action="search.php" method="GET" class="col-xs-12">
-            <input type="text" class="form-control" name="search" id="search" placeholder="Entrez votre recherche (Code Barre, Nom de produit, Catégorie ...)"><br/>
+            <input type="text" class="form-control" name="search" id="search" placeholder="Entrez votre recherche (Code Barre, Nom du produit, Catégorie ...)"><br/>
             <button type="submit" class="btn btn-default">Rechercher</button>
         </form>
     </div>
