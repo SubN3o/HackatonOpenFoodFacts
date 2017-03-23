320da170327972c19f4b56478fc88aca5f57b487
SubN3o <jeremie.daviaud@sfr.fr>
2017-03-23 23:49:43 +0100

    accueil css

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <DOCTYPE html>
 
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+              crossorigin="anonymous">
+        <link rel="stylesheet"  href="stylesheet.css">
+        <title>Manger, Wilder, Bouger.fr</title>
     </head>
     <body>
-        <form action="produit.php" method="POST">
-          <div class="form-group">
-            <label for="codebarre">Recherche par Code-Barres</label><br/>
-            <input type="text" class="form-control" name="id" id="codebarre" placeholder="Entrez le numéro de code-barres"><br/>
-          </div>
-          <button type="submit" class="btn btn-default">Rechercher</button>
-        </form>
+
+
+    <div class="container">
+        <div class="row">
+            <img class="logoMed" src="openfoodfacts-logo-med.png">
+            <form action="produit.php" method="POST" class="search">
+                <div class="form-group">
+                    <label for="codebarre">Recherche par Code-Barres</label>
+                    <input type="text" class="form-control" name="id" id="codebarre" placeholder="Entrez le numéro de code-barres"><br/>
+                    <button type="submit" class="btn btn-default">Rechercher</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+
+
     </body>
 
 </DOCTYPE>
--- produit.php
+++ produit.php
@@ -3,7 +3,10 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
+
+
 <?php
+include 'navbar.php';
 include 'bdd.php';
 $bdd = mysqli_connect(SERVER, USER, PASS, DB);
 $error = 0;
