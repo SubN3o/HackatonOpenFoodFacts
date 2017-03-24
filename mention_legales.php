51768fd007febf0feb9cf07ab163984d0ad1837f
Nico P <nicopinsard@gmail.com>
2017-03-24 11:32:33 +0100

    sdvcmfdo

--- mention_legales.php
+++ mention_legales.php
@@ -1,16 +1,25 @@
 <?php
-    $position = 'mentions';
+    $position = 'mention';
     include 'header.php';
+    include 'navbar.php';
     ?>
-
+<div class="container">
         <div class="row text-center mentions-legales">
             <p>
             <h1>La rançon est de 1.000.000 €<br/> pour avoir les mentions légales !!!</h1>
-            <h3> A partir du 21 Juillet 2017, nous avons besoin d'un stage ...<br/>
-            (La rémunération n'est pas obligatoire ...)</h3>
+            <h3> A partir du 21 Juillet 2017, nous avons besoin d'un stage de 3 mois ...<br/>
+            (La rémunération n'est pas obligatoire ...)<br/>
+            Vous pouvez nous contacter par l'école</h3>
             </p>
         </div>
+        <div class="row text-center">
+            <img class="img-mention" src="src/img/20170324_043651.jpg">
+            <img class="img-mention" src="src/img/20170324_043707.jpg">
+            <img class="img-mention" src="src/img/20170324_060643.jpg">
 
+
+        </div>
+</div>
 <?php
 include 'footer.php';
 
